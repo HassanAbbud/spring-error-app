3528b4ca8214e24f0cf8a8408c5d5651a6d4f13e
Hassan Abbud <hassanabbud@hotmail.com>
2024-08-09 17:23:24 -0600

    Handling error 404 not found

--- src/main/java/com/hassan/springboot/error/springboot_error/controllers/ExceptionHandlerController.java
+++ src/main/java/com/hassan/springboot/error/springboot_error/controllers/ExceptionHandlerController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.hassan.springboot.error.springboot_error.models.ErrorDTO;
 
@@ -21,5 +22,17 @@ public class ExceptionHandlerController {
         error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
 
         return ResponseEntity.internalServerError().body(error);
+    }
+
+    @ExceptionHandler({NoHandlerFoundException.class})
+    public ResponseEntity<ErrorDTO> notFoundException(Exception ex){
+        ErrorDTO error = new ErrorDTO();
+        error.setDate(new Date());
+        error.setError("Api rest not found!");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(404).body(error);
+
     }
 }
